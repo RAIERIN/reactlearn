0644ce2de79d92c1e24255381776acf0bad11f8f
RAIERIN <erinrai5874@gmail.com>
2019-11-10 16:52:44 +0545

    added packages required and action types are declared

---
+++ src/store/actions/action_types.js
@@ -0,0 +1,10 @@
+//action types
+
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const ADD_PROFILE = "ADD_PROFILE";
+export const REMOVE_PROFILE = "REMOVE_PROFILe";
+export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
+export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
